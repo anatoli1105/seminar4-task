8b1450bc43028b7b60615c1acce0ccdcc57235ef
anatolii <anatoliise@mail.ru>
2022-03-20 21:37:18 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int Promt(string messege)
+{
+    Console.WriteLine(messege);
+    string strN = Console.ReadLine();
+    int N =int.Parse(strN);
+    return N;
+}
+int number = Promt("введите число ");
+int degree = Promt("введите второе число ");
+int i = 1;
+int N = number;
+while(  i < degree)
+{
+     number = number * N;
+     i++;
+}
+Console.WriteLine(number);
+
+//or (int d = 1;d <= N;d++)
